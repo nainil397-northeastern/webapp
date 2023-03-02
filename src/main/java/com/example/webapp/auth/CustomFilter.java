3aa8bac687018e3f6b691c531d3d640f27ceb6bd
Nainil30 <maladkar.n@northeastern.edu>
2023-03-02 12:49:59 -0500

    update5

--- src/main/java/com/example/webapp/auth/CustomFilter.java
+++ src/main/java/com/example/webapp/auth/CustomFilter.java
@@ -1,13 +1,14 @@
-package com.example.webapp.authorisation;
+package com.example.webapp.auth;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import org.springframework.web.filter.GenericFilterBean;
+
 import java.io.IOException;
 
-public class CustomFilter extends GenericFilterBean {
+public class CustomFilter  extends GenericFilterBean {
 
     @Override
     public void doFilter(
