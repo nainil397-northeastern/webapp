222be63ef5a3c8918c3b001c67b2aa294b41f776
Nainil30 <maladkar.n@northeastern.edu>
2023-03-31 00:26:52 -0400

    chages to filter file for image

--- src/main/java/com/example/webapp/auth/CustomFilter.java
+++ src/main/java/com/example/webapp/auth/CustomFilter.java
@@ -43,7 +43,7 @@ public class CustomFilter  extends GenericFilterBean {
             ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
 
-        if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/v1/user/")){
+       if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/v1/user/")){
 
             statsd.incrementCounter("endpoint.get.v1.user");
             logger.info("Entered: GET /v1/user/userId");
@@ -52,26 +52,6 @@ public class CustomFilter  extends GenericFilterBean {
 
             statsd.incrementCounter("endpoint.put.v1.user");
             logger.info("Entered: PUT /v1/user/userId");
-
-        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("POST") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product")){
-
-            statsd.incrementCounter("endpoint.post.v1.product");
-            logger.info("Entered: POST /v1/product");
-
-        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("PUT") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")){
-
-            statsd.incrementCounter("endpoint.put.v1.product");
-            logger.info("Entered: PUT /v1/product/productId");
-
-        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("PATCH") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")){
-
-            statsd.incrementCounter("endpoint.patch.v1.product");
-            logger.info("Entered: PATCH /v1/product/productId");
-
-        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("DELETE") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")){
-
-            statsd.incrementCounter("endpoint.delete.v1.product");
-            logger.info("Entered: DELETE /v1/product/productId");
 
         }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("POST") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/") && ((HttpServletRequest)request).getRequestURI().contains("/image")){
 
@@ -93,6 +73,26 @@ public class CustomFilter  extends GenericFilterBean {
             statsd.incrementCounter("endpoint.delete.v1.product.image");
             logger.info("Entered: DELETE /v1/product/productId/image/imageId");
 
+        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("POST") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product")){
+
+            statsd.incrementCounter("endpoint.post.v1.product");
+            logger.info("Entered: POST /v1/product");
+
+        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("PUT") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")){
+
+            statsd.incrementCounter("endpoint.put.v1.product");
+            logger.info("Entered: PUT /v1/product/productId");
+
+        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("PATCH") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")){
+
+            statsd.incrementCounter("endpoint.patch.v1.product");
+            logger.info("Entered: PATCH /v1/product/productId");
+
+        }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("DELETE") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")) {
+
+            statsd.incrementCounter("endpoint.delete.v1.product");
+            logger.info("Entered: DELETE /v1/product/productId");
+
         }else if(((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/healthz")){
 
             statsd.incrementCounter("endpoint.get.v1.healthz");
@@ -110,6 +110,19 @@ public class CustomFilter  extends GenericFilterBean {
 
         }else{
             int i = 0;
+        }
+
+if((((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/v1/user/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("PUT") && ((HttpServletRequest)request).getRequestURI().contains("/v1/user/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("POST") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/") && ((HttpServletRequest)request).getRequestURI().contains("/image")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/") && ((HttpServletRequest)request).getRequestURI().contains("/image/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("GET") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/") && ((HttpServletRequest)request).getRequestURI().contains("/image")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("DELETE") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/") && ((HttpServletRequest)request).getRequestURI().contains("/image/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("POST") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("PUT") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("PATCH") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/")) ||
+                (((HttpServletRequest)request).getMethod().equalsIgnoreCase("DELETE") && ((HttpServletRequest)request).getRequestURI().contains("/v1/product/"))){
+            logger.info("User successfully authenticated.");
         }
 
         chain.doFilter(request, response);
